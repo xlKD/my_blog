731683642c1c539442cd21980badf00307dd914f
NGUYEN QUOC HUNG <hungxlkd@gmail.com>
2018-10-14 00:09:01 +0900

    Adapt to v3 MongoClient.connect

--- routes/posts.js
+++ routes/posts.js
@@ -1,30 +1,31 @@
 var express = require('express');
 var router = express.Router();
-var db = require('./db');
+var MongoClient = require('mongodb').MongoClient
+
+var conn = MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true })
 
 router.get('/', function(req, res, next) {
-	db.collection('blogs').find().toArray(function(err, results) {
+	conn.then(client => client.db('blog').collection('blogs').find().toArray(function(err, results) {
   		console.log(results)
 
         res.render('post/index', {
             blogs: results
         })
-	})
-});
+	}))
+})
 
 router.get('/add', function(req, res, next) {
     res.render('post/add', {
-        blogs: results
 	})
 });
 
 router.post('/create', function(req, res, next) {
-    db.collection('blogs').save(req.body, (err, result) => {
+    conn.then(client => client.db('blog').collection('blogs').insertOne(req.body, (err, result) => {
         if (err) return console.log(err)
 
         console.log('saved')
-        res.redirect('/')
-    })
+        res.redirect('/posts')
+    }))
 });
 
 module.exports = router;
