93b25aabfc8804bb2c83d0706bd41ccc8fafc8a9
NGUYEN QUOC HUNG <hungxlkd@gmail.com>
2018-10-17 23:46:30 +0900

    Basic CR(UD) working

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var MongoClient = require('mongodb').MongoClient
+var ObjectID = require('mongodb').ObjectID;
 
 var conn = MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true })
 
@@ -14,16 +15,26 @@ router.get('/', function(req, res, next) {
 	}))
 })
 
+router.get('/:post_id', function(req, res, next) {
+    const post_id = req.params.post_id.toString()
+	conn.then(client => client.db('blog').collection('blogs').find({_id: new ObjectID(post_id)}).limit(1).next(function(err, post) {
+        if (err) return console.log(err)
+
+        res.render('post/detail', {
+	        post: post
+        })
+    }))
+});
+
 router.get('/add', function(req, res, next) {
     res.render('post/add', {
 	})
 });
 
-router.post('/create', function(req, res, next) {
+router.post('/add', function(req, res, next) {
     conn.then(client => client.db('blog').collection('blogs').insertOne(req.body, (err, result) => {
         if (err) return console.log(err)
 
-        console.log('saved')
         res.redirect('/posts')
     }))
 });
