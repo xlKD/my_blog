62ff9c4cacade19b122cc59c752857f29a2517d7
NGUYEN QUOC HUNG <hungxlkd@gmail.com>
2019-09-20 23:41:18 +0900

    Test webhook

--- routes/webHooks.js
+++ routes/webHooks.js
@@ -4,23 +4,20 @@ const crypto = require('crypto');
 const childProcess = require('child_process');
 
 router.post('/pushed_master', function(req, res, next) {
+    let isSuccess = false;
     const secret = process.env.WEBHOOK_SECRET;
     const sig = "sha1=" + crypto.createHmac('sha1', secret).update(JSON.stringify(req.body)).digest('hex');
 
     if ( req.headers['x-hub-signature'] === sig ) {
-        console.log('IN');
-        childProcess.exec('cd /home/hung/Code && ./pull_and_restart_server.sh', function(err, stdout, stderr) {
-            console.log('INSIDE');
-            console.log(stdout);
-            console.log(stderr);
+        isSuccess = childProcess.exec('cd /home/hung/Code && ./pull_and_restart_server.sh', function(err, stdout, stderr) {
             if ( err ) {
-                console.log('ERR!');
-                return res.sendStatus(500);
+                return false;
             }
-            console.log('OK');
-            return res.sendStatus(200);
+            return true;
         });
     }
+
+    isSuccess ? res.sendStatus(200) : res.sendStatus(500);
 });
 
 router.post('/pushed_master_client', function(req, res, next) {
